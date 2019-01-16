8bc2222a7fe6510aa6c634dd23b7fe41a684dd60
avolosko <andrew.volosko@vakoms.com.ua>
2019-01-16 18:42:25 +0200

    ADDED: always use serialization keys to prevent issues during pro guard obfuscation

--- app/src/main/java/com/avolosko/spacex/api/pojo/RocketResponse.kt
+++ app/src/main/java/com/avolosko/spacex/api/pojo/RocketResponse.kt
@@ -4,9 +4,9 @@ import com.google.gson.annotations.SerializedName
 
 data class RocketResponse(
     @SerializedName("rocket_id") val id: String,
-    val active: Boolean,
+    @SerializedName("active") val active: Boolean,
     @SerializedName("rocket_name") val name: String,
-    val description: String,
-    val country: String,
-    val engines: EnginesResponse
+    @SerializedName("description") val description: String,
+    @SerializedName("country") val country: String,
+    @SerializedName("engines") val engines: EnginesResponse
 )
